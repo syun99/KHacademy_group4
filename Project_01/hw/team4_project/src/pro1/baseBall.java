787d083bc4ad7c80fe1718c0d348322153a936ea
kohyunwo <48508102+kohyunwo@users.noreply.github.com>
2019-03-26 15:59:27 +0900

    수정

--- Project_01/hw/team4_project/src/pro1/baseBall.java
+++ Project_01/hw/team4_project/src/pro1/baseBall.java
@@ -7,37 +7,29 @@ public class baseBall {
 	public baseBall() {
 		Random ran = new Random();
 		Scanner sc = new Scanner(System.in);
-		
-		String input_str = null;
-		char input_ch='\0';
-		
-		
+
+		int chance=0;
 		System.out.println("\n\t>> [숫자 야구 게임 START!!]");
-		while(input_ch<51 || input_ch > 57) {
+		while(chance<3 || chance>9) {
 			System.out.println("\n[3~9 까지 가능] ");
 			System.out.print("몇개의 공을 치시겠습니까?");
-			input_str = sc.nextLine();
-			if( input_str.length() ==1) {
-				input_ch = input_str.charAt(0);
-				if(input_ch<51 || input_ch > 57) 
-					System.out.println("\n\t>>다시입력!!");	
-			} else System.out.println("\n\t>>다시입력!!");
-			
+			chance = sc.nextInt();
+
+			if(chance<3 || chance>9) System.out.println("\n\t다시 입력해주세요!!");
 		}
-		
 		int max = 0;
-		
-		for(int i=49; i<=(int)input_ch; i++)	max++;
-			
+		max = chance;
+
 		int[] com_num = new int[max];
 		int[] user_num = new int[max];
 		int[] strike_num = new int[max];
+		int num=0;
 
 		int strike =0;
 		int ball = 0;
 		int out = 0;
 		int round=1;
-		
+
 		for(int i=0; i<max; i++) {
 			com_num[i] = ran.nextInt(9)+1;
 			for(int j=0; j<i; j++) {
@@ -46,28 +38,26 @@ public class baseBall {
 				}
 			}
 		}
-		
+
 		System.out.println("\n\n------------- 게임 규칙 -------------");
 		System.out.println("스트라이크(STRIKE): 숫자와 위치가 모두 맞음");
 		System.out.println("볼(BALL): 숫자는 맞지만 위치가 틀림");
 		System.out.println("아웃(OUT): 숫자와 위치가 모두 틀림");
 		System.out.println("홈런(HOMERUN): 수비팀의 숫자를 정확히 맞힘");
 		System.out.println("--------------------------------");
-		
+
 		while(true) {
 			System.out.println("\n\n[ROUND"+round+"]");
 			System.out.println("1~9까지의 숫자를 "+max+"번 입력해 주세요");
 			for(int i=0; i<max; i++) {
-				input_ch='\0';
-				while(input_ch<49 || input_ch>57) {
+				while(num<1 || num>9) {
 					System.out.print((i+1)+"번째 수 입력 >> ");
-					input_str=sc.nextLine();
-					if(input_str.length()==1) {
-						input_ch = input_str.charAt(0);
-						if(input_ch<49 || input_ch>57)	System.out.println("\n\t>>다시 입력!!\n");
-					} else System.out.println("\n\t>>다시 입력!!\n");
+					num=sc.nextInt();
+					if(num<1 || num>9) System.out.println("\n\t>>다시 입력해 주세요!!\n");
+
 				}
-				user_num[i] = Integer.parseInt(input_str);
+				user_num[i] = num;
+				num=0;
 			}
 			for(int i=0; i<max; i++) {
 				for(int j=0; j<max; j++) {
@@ -77,6 +67,7 @@ public class baseBall {
 							strike_num[i] = user_num[j];
 							if( strike == max) {
 								System.out.println("\n\t>>"+round+"라운드 홈런~~\n");
+
 								return;
 							}
 						}
